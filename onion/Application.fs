0e99a8b899d200a4525852fd8feaab4f06227c2d
borar <borar@somewhere.org>
2022-10-31 17:38:30 +0100

    Started the application layer with retrieval of all todo items

---
+++ onion/Application.fs
@@ -0,0 +1,13 @@
+﻿module Application
+
+open Model
+
+module GetAllTodos =
+    
+    type IDataAccess =
+
+        abstract member LoadAllTodos : Async<List<Todo>>
+
+    let getAllTodos (dataAccess : IDataAccess) =
+        // Just a pass-through to the DAL
+        dataAccess.LoadAllTodos
