65e8e9fa57dbaf066bcc8c71545e01e5d57a5bab
borar <borar@somewhere.org>
2022-10-31 17:28:47 +0100

    Basic port of the model/domain layer to the new Onion module

---
+++ onion/Library.fs
@@ -0,0 +1,20 @@
+﻿[<AutoOpen>]
+module Library
+
+[<RequireQualifiedAccess>]
+module Result =
+
+    let ofTryParse originalValue v =
+        match v with
+        | true, v -> Ok v
+        | false, _ -> Error $"Parse failed: '{originalValue}'"
+
+[<RequireQualifiedAccess>]
+module Option =
+
+    /// If value is None, returns Ok None, otherwise runs the validator on Some value and wraps the result in Some. Use
+    /// this if you want to handle the case for optional data, when you want to validate data *only if there is some*.
+    let toResultOption validator value =
+        value
+        |> Option.map (validator >> Result.map Some)
+        |> Option.defaultWith (fun () -> Ok None)
---
+++ onion/Model.fs
@@ -0,0 +1,70 @@
+﻿module Model
+
+open System
+open FsToolkit.ErrorHandling
+
+type SafeString =
+    static member TryCreate(value: string, builder, ?minLength, ?maxLength) =
+        match value, minLength, maxLength with
+        | null, _, _ -> Error "No value provided."
+        | value, Some min, _ when value.Length < min ->
+            Error $"Field is too short (min is {min}, but was {value.Length})."
+        | value, _, Some max when value.Length > max ->
+            Error $"Field is too long (max is {max}, but was {value.Length})."
+        | value, _, _ -> Ok(builder value)
+
+type String255 =
+    private
+    | String255 of string
+
+    static member TryCreate value =
+        SafeString.TryCreate(value, String255, maxLength = 255)
+
+    member this.Value =
+        match this with
+        | String255 v -> v
+
+type TodoId =
+    private
+    | TodoId of Guid
+
+    member this.Value =
+        match this with
+        | TodoId v -> v
+
+    static member TryCreate todoId =
+        if todoId = Guid.Empty then
+            Error "This is the empty GUID."
+        else
+            Ok (TodoId todoId)
+
+    static member TryCreate(guid: string) =
+        guid |> Guid.TryParse |> Result.ofTryParse guid |> Result.map TodoId
+
+(* Not the absence of `Guid.NewGuid()` for `TodoId`. *)
+
+type Todo =
+    {
+        Id: TodoId
+        Title: String255
+        Description: String255 option
+        CreatedDate: DateTime
+        CompletedDate: DateTime option
+    }
+
+    static member TryCreate(id, title, description, createdOn) = validation {
+        let! title = title |> Result.tryCreate "Title"
+
+        and! description =
+            description
+            |> Option.ofObj
+            |> Option.toResultOption (Result.tryCreate "Description")
+
+        return {
+            Id = id
+            Title = title
+            Description = description
+            CreatedDate = createdOn
+            CompletedDate = None
+        }
+    }
