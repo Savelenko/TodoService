821ad16cda1af0a2ded1350a19fe66df436f4eea
borar <borar@somewhere.org>
2022-11-01 12:35:40 +0100

    Service layer for the "get all todos" use-case and the CLI integratin of all layers

--- onion/Program.fs
+++ onion/Program.fs
@@ -1,10 +1,58 @@
-﻿//
+﻿open System
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.AspNetCore.Builder
+open Giraffe
+open Microsoft.Extensions.Logging
+open Microsoft.Extensions.Hosting
+open Microsoft.AspNetCore.Authentication.Cookies
+open Microsoft.Extensions.Configuration
+open Microsoft.AspNetCore.Hosting
+
 open Application
 open DataAccess
 
+// Normally the connection string would be in the configuration settings file
 let database = ConnectionString "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Todo;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
 
+/// Assembles the ASP.NET "application" from various framework modules.
+let private configureApp (app: IApplicationBuilder) =
+    app
+        .UseHsts()
+        .UseGiraffeErrorHandler(WebService.errorHandler)
+        .UseStaticFiles()
+        .UseAuthentication()
+        .UseAuthorization()
+        // Provide Giraffe with combined HTTP handlers
+        .UseGiraffe(WebService.allServices)
+
+/// Prepares dependency injection consisting of framework and application-specific components.
+let private configureServices (services: IServiceCollection) =
+    services
+        // Some of the following stuff is probably not needed, but I just pasted this from somewhere else
+        .AddHsts(fun options -> options.MaxAge <- TimeSpan.FromDays(180.0))
+        .AddAuthorization()
+        .AddAuthentication(fun options -> options.DefaultScheme <- CookieAuthenticationDefaults.AuthenticationScheme)
+        .Services
+        // This is the place where the most outer layer chooses implementations of infrastructure layers (data access in
+        // this case) which will end up being passed to the application layer. A.k.a the composition root.
+        .AddSingleton<GetAllTodos.IDataAccess>(GetAllTodos.dataAccessImplementation database)
+        .AddGiraffe()
+        |> ignore
+
 [<EntryPoint>]
-let main argv =    
-    printfn "%A" (Async.RunSynchronously (GetAllTodos.getAllTodos (GetAllTodos.dataAccessImplementation database)))
-    0
+let main argv =
+    try
+        Host
+            .CreateDefaultBuilder(argv)
+            .ConfigureWebHostDefaults(fun webHostBuilder ->
+                webHostBuilder
+                    .ConfigureServices(configureServices)
+                    .Configure(configureApp)
+                |> ignore
+            )
+            .UseConsoleLifetime(fun opts -> opts.SuppressStatusMessages <- true)
+            .Build()
+            .Run()
+        0
+    with
+    | ex -> 1
---
+++ onion/WebService.fs
@@ -0,0 +1,35 @@
+﻿module WebService
+
+open System
+open Giraffe
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.Logging
+
+open Application
+
+(* Note that there is no dependency on the data access layer. Module order in the project emphasizes this. *)
+
+module GetAllTodos =
+
+    open GetAllTodos
+
+    let private getAllTodos next (ctx: HttpContext) = task {
+        // Retrieve data access implementation using .NET DI. This is the only place I use DI to avoid fighting against
+        // the platform; this is an outer layer (the "shell") after all, so it's OK. Alternatively, passing data access
+        // implementation as an argument to this function is also trivial and more explicit (and safe).
+        let dataAccess = ctx.GetService<IDataAccess> ()
+        // Invoke the use-case in the application layer
+        let! todos = GetAllTodos.getAllTodos dataAccess
+        // Serialize the result
+        return! json todos next ctx
+    }
+
+    let httpHandler : HttpHandler =  GET >=> route "/todo" >=> getAllTodos
+
+let errorHandler (ex: Exception) (log: ILogger) =
+    log.LogError(EventId (), ex, "An unhandled exception has occurred while executing the request.")
+    clearResponse >=> setStatusCode 500
+
+let allServices : HttpHandler = choose [
+    GetAllTodos.httpHandler
+]
